5311ac3666e609573accd02169528088fe5b3e1d
Matt Blissmer <mblissmer@users.noreply.github.com>
2026-08-05 10:14:15 -0400

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,29 @@ function searchSubtitles() {
     }
 
 
-    const matches = fuse.search(query);
+    const grouped = new Map();
+    
+    for (const result of fuse.search(query)) {
+    
+        const item = result.item;
+    
+        const key = item.file;
+    
+        if (!grouped.has(key)) {
+            grouped.set(key, {
+                season: item.season,
+                episode: item.episode,
+                title: item.title,
+                file: item.file,
+                hits: []
+            });
+        }
+    
+        grouped.get(key).hits.push({
+            time: item.time,
+            text: item.text
+        });
+    }
 
 
     if (matches.length === 0) {
@@ -51,36 +73,35 @@ function searchSubtitles() {
 
 
     let html = "";
-
-    for (const match of matches.slice(0, 50)) {
-
-        const item = match.item;
-
-
+    
+    for (const episode of grouped.values()) {
+    
         html += `
-        <hr>
-
-        <p>
-        <b>
-        S${String(item.season).padStart(2,"0")}
-        E${String(item.episode).padStart(2,"0")}
-        - ${item.title}
-        </b>
-        </p>
-
-        <p>
-        <small>${item.time}</small>
-        </p>
-
-        <blockquote>
-        ${item.text}
-        </blockquote>
-
-        <p>
-        <a href="${item.file}">
-        Download subtitle
-        </a>
-        </p>
+            <hr>
+    
+            <h3>
+                S${String(episode.season).padStart(2,"0")}
+                E${String(episode.episode).padStart(2,"0")}
+                - ${episode.title}
+            </h3>
+        `;
+    
+        for (const hit of episode.hits) {
+    
+            html += `
+                <div class="hit">
+                    <b>${hit.time}</b>
+                    ${hit.text}
+                </div>
+            `;
+        }
+    
+        html += `
+            <p>
+                <a href="${episode.file}">
+                    Download subtitle
+                </a>
+            </p>
         `;
     }
 
